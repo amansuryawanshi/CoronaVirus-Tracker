0b0b11e8997bc5c945e1dad567e2536b714c1a2c
amansuryawanshi <52121580+amansuryawanshi@users.noreply.github.com>
2021-05-26 18:57:56 +0530

    Add files via upload

--- src/Components/stateWiseData/statewise.js
+++ src/Components/stateWiseData/statewise.js
@@ -1,13 +1,59 @@
-import React from 'react'
-
+import React, {useEffect, useState} from 'react'
+import './statewise.css';
 const Statewise = () => {
-    return ( < >
+    const [data,setData] = useState([]);
+    const getCovidDatastate =  async () =>{
+        try {
+            const res = await fetch('https://api.covid19india.org/data.json');
+            const actualDatastate = await res.json();
+            console.log(actualDatastate.statewise);
+            setData(actualDatastate.statewise);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        getCovidDatastate();
+    }, []);
+    return (
+        <>
+        <section>
+            
+           <div className="container  mt-5">
+              <table className="table  table-bordered  ">
+                <thead className="thead">
+                    <tr>
+                        <th>State</th>
+                        <th>Confirmed</th>
+                        <th>Recovered</th>
+                        <th>Deaths</th>
+                        <th>Active</th>
+                        <th>Updated on</th>
+                    </tr>
 
-        <
-        h1 > India Covid Data < /h1>
-
-        <
-        />
+                </thead>
+                <tbody>
+                    {
+                            data.map((curElem, ind) => {
+                                return(
+                                    <tr key={ind}>
+                                    <td className="data">{curElem.state}</td>
+                                    <td className="confirmed">{curElem.confirmed}</td>
+                                    <td className="recovered">{curElem.recovered}</td>
+                                    <td className="deaths">{curElem.deaths}</td>
+                                    <td className="active">{curElem.active}</td>
+                                    <td className="data">{curElem.lastupdatedtime}</td>
+                                    </tr>
+                                );
+                            })
+                    }
+                   
+                </tbody>
+              </table>
+               </div>
+           
+           </section>
+        </>
     )
 }
 
