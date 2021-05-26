e33de29997bd95df40120a746f4d8157c02460aa
amansuryawanshi <52121580+amansuryawanshi@users.noreply.github.com>
2021-05-26 18:58:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Covid from './Components/covid';
+import Statewise from './Components/stateWiseData/statewise'
 import './App.css';
 
 const App = () => {
     return ( <>  
     <Covid/>
+    <Statewise/>
         </>
     );
 }
